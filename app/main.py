8bf007e22eb8a8cc36574927cedbabbdc5d9bd1e
Farid Omar <fariedomar43@gmail.com>
2026-02-08 18:41:55 +0100

    Update app/main.py to check DB health route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+
 from app.core.config import settings
-from app.api.routes import health_router
+from app.api.routes import health_router, db_health_router
 
 app = FastAPI(title=settings.app_name)
 
 app.include_router(health_router)
+app.include_router(db_health_router)
