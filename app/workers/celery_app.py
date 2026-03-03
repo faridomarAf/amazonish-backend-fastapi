6e1f739b24fc7ca3505de37b656fc1f96df3a5e1
Farid Omar <fariedomar43@gmail.com>
2026-03-03 06:40:32 +0100

    create [app/workers/celery_app.py] file

---
+++ app/workers/celery_app.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+celery_app = Celery(
+    "amazonish",
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/0",
+)
+
+celery_app.conf.task_routes = {
+    "app.workers.tasks.*": {"queue": "default"},
+}
