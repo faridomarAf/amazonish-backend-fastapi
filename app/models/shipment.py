9044b73e6401894cb14abebcfb7e21799e280a39
Farid Omar <fariedomar43@gmail.com>
2026-03-05 06:48:56 +0100

    Create Migration for shipment table

--- app/models/shipment.py
+++ app/models/shipment.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, text
+from sqlalchemy import Column, BigInteger, String, ForeignKey, text
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.mysql import DATETIME
 from datetime import datetime
@@ -8,8 +8,8 @@ from app.db.base import Base
 class Shipment(Base):
     __tablename__ = "shipments"
 
-    id = Column(Integer, primary_key=True)
-    order_id = Column(Integer, ForeignKey("orders.id"))
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    order_id = Column(BigInteger, ForeignKey("orders.id"))
     status = Column(String(50), default="CREATED")
     carrier = Column(String(100))
     tracking_number = Column(String(100))
